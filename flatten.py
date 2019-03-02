3817d0b2ccbf91543f106029a8fda3bccd99bbe8
BlucyBlue <46870784+BlucyBlue@users.noreply.github.com>
2019-03-02 15:46:53 +0100

    Update flatten.py

--- flatten.py
+++ flatten.py
@@ -1,13 +1,4 @@
 """
-I needed to extract nested lists for a personal project, so I came up with a couple of solutions.
-
-Should be easy to customize and modify result format as needed.
-
-If you find the need to, use as you wish.
-"""
-
-
-"""
 Flattens all nested lists (all levels), but does not extract members of other iterables.
 For eficiensy in working with larger datasets, returns a generator object.
 """
